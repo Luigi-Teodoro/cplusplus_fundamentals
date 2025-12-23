287ecba16733e12b25137d47ccfcf750027464e5
Luigi-Teodoro <luigi.tc14@gmail.com>
2025-12-22 19:33:59 -0500

    primer commit

---
+++ Imprimir_mensajes/ejercicios_imprimir.cpp
@@ -0,0 +1,60 @@
+
+//Ejercicios realizado para imprimir mensajes
+//Para ejecutar alguno tenemos que comentar los demas main
+
+#include <iostream>
+
+int main(){
+    std::cout<<"Usuario: Luigi\nRol: Estudiante\nEstado: Activo";
+    return 0;
+}
+
+
+
+int main(){
+    std::cout<<"[ OK ] Sistema listo";
+    return 0;
+}
+
+
+
+
+int main(){
+    std::cout<<"+------------+"<<'\n';
+    std::cout<<"|  C++       |"<<'\n';
+    std::cout<<"+------------+";
+    return 0;
+}
+
+
+
+int main(){
+    std::cout<<"Ruta del archivo: C:\\Users\\Luigi\\Documentos";
+    return 0;
+}
+
+
+
+int main(){
+    std::cout<<"El mensaje fue: \"Acceso concedido\"";
+    return 0;
+}
+
+
+
+int main(){
+    std::cout<<"Linea A\nLinea B\nLinea C";
+    return 0;
+}
+
+
+
+
+int main(){
+    std::cout<<"#\n##\n###\n####";
+    return 0;
+}
+
+
+
+
---
+++ Imprimir_mensajes/imprimir_texto.cpp
@@ -0,0 +1,59 @@
+
+//De esta manera se imprimen mensajes en c++
+//Estructura para imprimir un mensaje 
+
+//#include <iostream>
+
+/*
+int main() { 
+    std::cout<<"Luigi Sebastian"<<'\n';  
+    std::cout<<"Aprender c++";
+    return 0;
+}
+*/
+
+
+//CONCEPTOS A TENER EN CUENTA PARA IMPRIMIR MENSAJES EN C++
+
+
+//Incluir el paquete iostream para imprimir mensajes
+#include <iostream>
+
+/*
+int main(){
+    std::cout<<"======================="<<'\n';
+    std::cout<<"  BIENVENIDO AL SISTEMA"<<'\n';
+    std::cout<<"=======================";
+    return 0;
+}
+*/
+
+/*
+int main(){
+    std::cout<<"Nombre: Luigi"<<'\n';
+    std::cout<<"Estado: Practicando C++"<<'\n';
+    std::cout<<"Lenguaje: C++";
+    return 0;
+}
+*/
+
+
+/*
+int main(){
+    std::cout<<"El profesor dijo: \"Practiquen C++ todos los dias\""; <--- Asi podemos escapar comillas en c++
+    return 0;
+}
+*/
+
+
+
+
+/*
+int main(){
+    std::cout<<"Linea uno"<<std::endl; <--- Otra manera de hacer espacion en c++
+    std::cout<<"Linea dos"<<std::endl;
+    return 0;
+}
+*/
+
+
